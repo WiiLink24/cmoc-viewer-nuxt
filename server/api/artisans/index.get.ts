eb20dd8e3837c97fa645b34a44d14c64ae546c25
kouacc <maxrur57@gmail.com>
2025-05-22 16:38:14 +0200

    chore: finish /api/artisans route, add openAPI spec

--- server/api/artisans/index.get.ts
+++ server/api/artisans/index.get.ts
@@ -1,5 +1,25 @@
-import { sql } from "drizzle-orm/sql";
+import { count, sql } from "drizzle-orm/sql";
 import { db } from "~/drizzle";
+import { artisans } from "~/drizzle/schema";
+
+defineRouteMeta({
+  openAPI: {
+    tags: ["Artisans"],
+    summary: "Get the newest Artisans",
+    description: "Returns 50 of the newest Artisans available",
+    responses: {
+      "200": {
+        description: "List returned",
+        content: {
+          "application/json": {
+            schema: {
+              type: "array",
+            },
+          },
+        },
+      },
+  }}
+});
 
 export default defineEventHandler(async (event) => {
     const { page } = getQuery<{ page?: string }>(event);
@@ -9,14 +29,23 @@ export default defineEventHandler(async (event) => {
     // limit to 50 items displayed per page
     const offset = (pageNumber - 1) * 50;
 
-    const artisans = await db.query.artisans.findMany({
+    const query = await db.query.artisans.findMany({
       orderBy: (artisans, { asc }) => [asc(artisans.artisanId)],
       offset: sql.placeholder("pOffset"),
       limit: 50,
     }).prepare("artisans");
 
-    const data = await artisans.execute({ pOffset: offset });
+    const data = await query.execute({ pOffset: offset });
 
+    const artisansData = data.map((row) => {
+        return {
+            ...row,
+            miiData: row.miiData ? convertToB64(row.miiData as unknown as Uint8Array) : "",
+        };
+    })
 
+    const total_items = await db.select({ count: count() }).from(artisans);
+    const total_pages = Math.ceil(total_items[0].count / 50)
 
+    return { total_pages: total_pages, data: artisansData };
 });
